e14c792225384fd1634e891087edddea7c203298
guoqw <412171946@qq.com>
2017-02-14 13:48:00 +0800

    format access log

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/AccessLogFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/AccessLogFilter.java
@@ -56,27 +56,38 @@ import com.alibaba.dubbo.rpc.RpcException;
  *    &lt;appender-ref ref="foo" /&gt;
  * &lt;/logger&gt;
  * </pre></code>
- * 
+ *
  * @author ding.lid
  */
 @Activate(group = Constants.PROVIDER, value = Constants.ACCESS_LOG_KEY)
 public class AccessLogFilter implements Filter {
-    
-    private static final Logger logger            = LoggerFactory.getLogger(AccessLogFilter.class);
 
-    private static final String  ACCESS_LOG_KEY   = "dubbo.accesslog";
-    
-    private static final String  FILE_DATE_FORMAT   = "yyyyMMdd";
+    private static final Logger logger = LoggerFactory.getLogger(AccessLogFilter.class);
 
-    private static final String  MESSAGE_DATE_FORMAT   = "yyyy-MM-dd HH:mm:ss";
+    private static final String ACCESS_LOG_KEY = "dubbo.accesslog";
+
+    private static final String FILE_DATE_FORMAT = "yyyyMMdd";
+
+    private static final String MESSAGE_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     private static final int LOG_MAX_BUFFER = 5000;
 
     private static final long LOG_OUTPUT_INTERVAL = 5000;
 
+    private static final String CLASS_NAME_PREFIX = "CLASS_NAME=";
+
+    private static final String TIME_PREFIX = "TIME=";
+
+    private static final String PRAMETERS_PREFIX = "PRAMETERS=";
+
+    private static final String RETRUN_PREFIX = "RETRUN=";
+
+    private static final String OTHER_INFO_PREFIX = "OTHER=";
+
     private final ConcurrentMap<String, Set<String>> logQueue = new ConcurrentHashMap<String, Set<String>>();
 
-    private final ScheduledExecutorService logScheduled = Executors.newScheduledThreadPool(2, new NamedThreadFactory("Dubbo-Access-Log", true));
+    private final ScheduledExecutorService logScheduled = Executors.newScheduledThreadPool(2,
+            new NamedThreadFactory("Dubbo-Access-Log", true));
 
     private volatile ScheduledFuture<?> logFuture = null;
 
@@ -90,7 +101,7 @@ public class AccessLogFilter implements Filter {
                             Set<String> logSet = entry.getValue();
                             File file = new File(accesslog);
                             File dir = file.getParentFile();
-                            if (null!=dir&&! dir.exists()) {
+                            if (null != dir && !dir.exists()) {
                                 dir.mkdirs();
                             }
                             if (logger.isDebugEnabled()) {
@@ -98,17 +109,18 @@ public class AccessLogFilter implements Filter {
                             }
                             if (file.exists()) {
                                 String now = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date());
-                                String last = new SimpleDateFormat(FILE_DATE_FORMAT).format(new Date(file.lastModified()));
-                                if (! now.equals(last)) {
+                                String last = new SimpleDateFormat(FILE_DATE_FORMAT).format(
+                                        new Date(file.lastModified()));
+                                if (!now.equals(last)) {
                                     File archive = new File(file.getAbsolutePath() + "." + last);
                                     file.renameTo(archive);
                                 }
                             }
                             FileWriter writer = new FileWriter(file, true);
                             try {
-                                for(Iterator<String> iterator = logSet.iterator();
-                                    iterator.hasNext();
-                                    iterator.remove()) {
+                                for (Iterator<String> iterator = logSet.iterator();
+                                     iterator.hasNext();
+                                     iterator.remove()) {
                                     writer.write(iterator.next());
                                     writer.write("\r\n");
                                 }
@@ -126,17 +138,18 @@ public class AccessLogFilter implements Filter {
             }
         }
     }
-    
+
     private void init() {
         if (logFuture == null) {
             synchronized (logScheduled) {
                 if (logFuture == null) {
-                    logFuture = logScheduled.scheduleWithFixedDelay(new LogTask(), LOG_OUTPUT_INTERVAL, LOG_OUTPUT_INTERVAL, TimeUnit.MILLISECONDS);
+                    logFuture = logScheduled.scheduleWithFixedDelay(new LogTask(), LOG_OUTPUT_INTERVAL,
+                            LOG_OUTPUT_INTERVAL, TimeUnit.MILLISECONDS);
                 }
             }
         }
     }
-    
+
     private void log(String accesslog, String logmessage) {
         init();
         Set<String> logSet = logQueue.get(accesslog);
@@ -154,23 +167,18 @@ public class AccessLogFilter implements Filter {
         try {
             String accesslog = invoker.getUrl().getParameter(Constants.ACCESS_LOG_KEY);
             if (ConfigUtils.isNotEmpty(accesslog)) {
+                // 记录开始时间
+                long start = System.currentTimeMillis();
+
                 RpcContext context = RpcContext.getContext();
                 String serviceName = invoker.getInterface().getName();
                 String version = invoker.getUrl().getParameter(Constants.VERSION_KEY);
                 String group = invoker.getUrl().getParameter(Constants.GROUP_KEY);
                 StringBuilder sn = new StringBuilder();
-                sn.append("[").append(new SimpleDateFormat(MESSAGE_DATE_FORMAT).format(new Date())).append("] ").append(context.getRemoteHost()).append(":").append(context.getRemotePort())
-                .append(" -> ").append(context.getLocalHost()).append(":").append(context.getLocalPort())
-                .append(" - ");
-                if (null != group && group.length() > 0) {
-                    sn.append(group).append("/");
-                }
-                sn.append(serviceName);
-                if (null != version && version.length() > 0) {
-                    sn.append(":").append(version);
-                }
-                sn.append(" ");
-                sn.append(inv.getMethodName());
+
+                // 拼接CLASS_NAME
+                sn.append(CLASS_NAME_PREFIX).append(serviceName);
+                sn.append(".").append(inv.getMethodName());
                 sn.append("(");
                 Class<?>[] types = inv.getParameterTypes();
                 if (types != null && types.length > 0) {
@@ -184,17 +192,46 @@ public class AccessLogFilter implements Filter {
                         sn.append(type.getName());
                     }
                 }
-                sn.append(") ");
+                sn.append(")");
+                if (null != version && version.length() > 0) {
+                    sn.append(":").append(version);
+                }
+                sn.append(",");
+
+                // 拼接入参 -- PRAMETERS
+                sn.append(PRAMETERS_PREFIX);
                 Object[] args = inv.getArguments();
                 if (args != null && args.length > 0) {
                     sn.append(JSON.json(args));
                 }
+                sn.append(",");
 
-                // add by gqw 2017-01-20
-                // 添加日志出参
+                // 拼接出参 -- RETRUN
+                sn.append(RETRUN_PREFIX);
                 result = invoker.invoke(inv);
                 if (!result.hasException()) {
-                    sn.append(",return=>").append(JSON.json(result.getValue()));
+                    sn.append(JSON.json(result.getValue()));
+                }
+                sn.append(",");
+
+                // 拼接服务调用耗时 -- TIME -- 耗时放在首位
+                sn.insert(0, TIME_PREFIX + (System.currentTimeMillis() - start) + ",");
+
+                // 拼接其他参数
+                sn.append(OTHER_INFO_PREFIX)
+                        .append("[")
+                        .append(new SimpleDateFormat(MESSAGE_DATE_FORMAT).format(new Date()))
+                        .append("] ")
+                        .append(context.getRemoteHost())
+                        .append(":")
+                        .append(context.getRemotePort())
+                        .append(" -> ")
+                        .append(context.getLocalHost())
+                        .append(":")
+                        .append(context.getLocalPort())
+                        .append(" - ");
+                if (null != group && group.length() > 0) {
+                    sn.append(group);
                 }
 
                 String msg = sn.toString();
